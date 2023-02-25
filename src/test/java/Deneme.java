1c2df68f0389ca8078bf63f05d7b91001ec72f70
Erol Evren <100449806+erlevren@users.noreply.github.com>
2023-02-25 18:50:51 +0300

    V3

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,9 +1,5 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Dünya");
-
     }
 }
-//Restore --> Tamamen Working Space' i temizler
-//Stash --> Hafızaya alır
